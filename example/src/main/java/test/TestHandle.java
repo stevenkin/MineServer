6e0c0a48ccadb2c41bd60adb4b0c1f8844ce9d1a
StevenKin <2315797259@qq.com>
2016-05-04 10:59:39 +0800

    add example

---
+++ example/src/main/java/test/TestHandle.java
@@ -0,0 +1,39 @@
+package test;
+
+import me.stevenkin.http.mineserver.core.annotation.Controller;
+import me.stevenkin.http.mineserver.core.annotation.InitParameter;
+import me.stevenkin.http.mineserver.core.container.AbstractHandle;
+import me.stevenkin.http.mineserver.core.entry.HttpRequest;
+import me.stevenkin.http.mineserver.core.entry.HttpResponse;
+import me.stevenkin.http.mineserver.core.parser.HttpParser;
+
+import java.io.Writer;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by wjg on 16-4-30.
+ */
+@Controller(method = HttpParser.METHOD.GET,urlPatten = "/get",initParameters = {
+        @InitParameter(key="key1",value="value1"),
+        @InitParameter(key="key2",value="value2")
+})
+public class TestHandle extends AbstractHandle {
+    @Override
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+        Map<String,String> params = httpRequest.getParams();
+        for(Map.Entry<String,String> entry : params.entrySet()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
+        }
+        System.out.println();
+        Iterator<String> iterator = getInitParameterNames();
+        while(iterator.hasNext()){
+            String key = iterator.next();
+            System.out.println(key+":"+getInitParameter(key));
+        }
+        char[] chars = {'h','e','l','l','o',',','w','o','r','l','d'};
+        Writer writer = httpResponse.getWrite();
+        writer.write(chars);
+        writer.flush();
+    }
+}
